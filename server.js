23ce4d7effd464165b13ee29f5ea7c1daebc713b
MyManny <gmail.com>
2023-08-22 19:51:54 -0700

    404 page

--- server.js
+++ server.js
@@ -7,9 +7,11 @@ const PORT = process.env.PORT
 const app = express()
 
 // MIDDLEWARE
+app.use(express.static('public'))
 app.set('views', __dirname + '/views')
 app.set('view engine', 'jsx')
 app.engine('jsx', require('express-react-views').createEngine())
+
 
 // ROUTES
 app.get('/', (req, res) => {
@@ -20,7 +22,13 @@ res.send('Welcome to an Awesome App about Breads')
 const breadsController = require('./controllers/breads_controller.js')
 app.use('/breads', breadsController)
   
+// 404 Page
+app.get('*', (req, res) => {
+  res.send('404')
+})
+
 // LISTEN
 app.listen(PORT, () => {
   console.log('listening on port', PORT);
 })
+
