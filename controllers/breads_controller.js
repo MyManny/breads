7cc5dadcf40fd5e355bc316b622ed612b3af263d
MyManny <gmail.com>
2023-08-29 21:37:05 -0700

    linked mondo

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -4,14 +4,15 @@ const Bread = require('../models/bread.js')
 
 // INDEX
 breads.get('/', (req, res) => {
-    
-  res.render('Index',
-      {
-        breads: Bread,
-        title: 'Bread LIST'
-      }
-    )
+  Bread.find()
+      .then(foundBreads => {
+          res.render('index', {
+              breads: foundBreads,
+              title: 'Index Page'
+          })
+      })
 })
+
 
 // NEW
 breads.get('/new', (req, res) => {
@@ -19,38 +20,52 @@ breads.get('/new', (req, res) => {
 })
 
 // SHOW
-breads.get('/:arrayIndex', (req, res) => {
-  console.log(req.params.arrayIndex)
-  if (Bread[req.params.arrayIndex]) {
-    res.render('Show', {
-      bread: Bread[req.params.arrayIndex],
-      index: req.params.arrayIndex
-    })
+breads.get('/:id', (req, res) => {
+  if (req.params.id) {
+      Bread
+          .findById(req.params.id)
+          .then(bread => {
+              res.render(
+                  'Show',
+                  {
+                      bread: bread,
+                      id: req.params.id
+                  })
+          })
+          .catch(err => {
+              console.log(err)
+              res.render('NotFound')
+          })
   } else {
-    res.render('NotFound')
+      res.render('NotFound')
   }
+  //res.send(Bread[req.params.arrayIndex])
 })
+
 
 // CREATE
 breads.post('/', (req, res) => {
-  if (!req.body.image) {
-    req.body.image = 'https://images.unsplash.com/photo-1517686469429-8bdb88b9f907?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80'
+  if(!req.body.image) {
+      req.body.image = undefined 
   }
   if(req.body.hasGluten === 'on') {
     req.body.hasGluten = true
   } else {
     req.body.hasGluten = false
   }
-  req.body.image = false
-  Bread.insertMany([req.body])
+  Bread.create(req.body)
   res.redirect('/breads')
 })
 
+
+
 // DELETE
-breads.delete('/:arrayIndex', (req, res) => {
-  Bread.splice(req.params.arrayIndex, 1)
+breads.delete('/:id', (req, res) => {
+  console.log(re.params.id)
+  Bread.splice(req.params.id, 1)
   res.status(303).redirect('/breads')
 })
+
 // EDIT
 breads.get('/:arrayIndex/edit', (req, res) => {
   res.render('edit', {
